409cc28e3e8c221f0ba4869469a7ce4630a535c4
willkroboth <46540330+willkroboth@users.noreply.github.com>
2025-10-19 16:14:44 -0400

    Make suggestions example use `EntitySelectorArgument.OnePlayer`
    Move the example into the entity selector section rather than the player profile section, matching the argument used in the examples.
    
    Move non Paper/Spigot specific examples into the Bukkit reference code section to reduce duplicated information (only `PlayerProfileArgument` and `AsyncPlayerProfileArgument` are Paper/Spigot specific)

--- reference-code/paper/src/main/java/createcommands/arguments/types/EntitiesArguments.java
+++ reference-code/paper/src/main/java/createcommands/arguments/types/EntitiesArguments.java
@@ -2,59 +2,14 @@ package createcommands.arguments.types;
 
 import com.destroystokyo.paper.profile.PlayerProfile;
 import dev.jorel.commandapi.CommandAPICommand;
-import dev.jorel.commandapi.arguments.Argument;
 import dev.jorel.commandapi.arguments.AsyncPlayerProfileArgument;
-import dev.jorel.commandapi.arguments.EntitySelectorArgument;
-import dev.jorel.commandapi.arguments.EntityTypeArgument;
-import dev.jorel.commandapi.arguments.IntegerArgument;
-import dev.jorel.commandapi.arguments.PlayerProfileArgument;
-import dev.jorel.commandapi.arguments.SafeSuggestions;
-import dev.jorel.commandapi.executors.CommandArguments;
 import org.bukkit.Bukkit;
-import org.bukkit.entity.Entity;
-import org.bukkit.entity.EntityType;
-import org.bukkit.entity.Player;
 
-import java.util.Collection;
 import java.util.List;
 import java.util.concurrent.CompletableFuture;
 
 class EntitiesArguments {
     static {
-        // #region entitySelectorArgumentExample
-        new CommandAPICommand("remove")
-            // Using a collective entity selector to select multiple entities
-            .withArguments(new EntitySelectorArgument.ManyEntities("entities"))
-            .executes((sender, args) -> {
-                // Parse the argument as a collection of entities (as stated above in the documentation)
-                @SuppressWarnings("unchecked")
-                Collection<Entity> entities = (Collection<Entity>) args.get("entities");
-
-                sender.sendMessage("Removed " + entities.size() + " entities");
-                for (Entity e : entities) {
-                    e.remove();
-                }
-            })
-            .register();
-        // #endregion entitySelectorArgumentExample
-
-        // #region buildNoSelectorSuggestions
-        Argument<?> noSelectorSuggestions = new PlayerProfileArgument("target")
-            .replaceSafeSuggestions(SafeSuggestions.suggest(info ->
-                Bukkit.getOnlinePlayers().stream().map(Player::getPlayerProfile).toArray(PlayerProfile[]::new)
-            ));
-        // #endregion buildNoSelectorSuggestions
-
-        // #region noSelectorSuggestionsExample
-        new CommandAPICommand("warp")
-            .withArguments(noSelectorSuggestions)
-            .executesPlayer((player, args) -> {
-                Player target = (Player) args.get("target");
-                player.teleport(target);
-            })
-            .register();
-        // #endregion noSelectorSuggestionsExample
-
         // #region playedBeforeArgumentExample
         new CommandAPICommand("playedbefore")
             .withArguments(new AsyncPlayerProfileArgument("player"))
@@ -81,17 +36,5 @@ class EntitiesArguments {
             })
             .register();
         // #endregion playedBeforeArgumentExample
-
-        // #region entityTypeArgumentExample
-        new CommandAPICommand("spawnmob")
-            .withArguments(new EntityTypeArgument("entity"))
-            .withArguments(new IntegerArgument("amount", 1, 100)) // Prevent spawning too many entities
-            .executesPlayer((Player player, CommandArguments args) -> {
-                for (int i = 0; i < (int) args.get("amount"); i++) {
-                    player.getWorld().spawnEntity(player.getLocation(), (EntityType) args.get("entity"));
-                }
-            })
-            .register();
-        // #endregion entityTypeArgumentExample
     }
 }
